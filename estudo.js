708b209156ee913139b9b6de25bce2fb78df226a
vt6060 <108939534+vt6060@users.noreply.github.com>
2022-11-04 11:14:59 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,5 +1,5 @@
 let tabuada = 1;
-let nome = "Antonio";
+let nome = "Vitor";
 
 function escreva() {
     document.write("tabuada do " + tabuada + "<br>")
@@ -60,13 +60,17 @@ if (!Number(t)) {
     return
 }
 let r = c;
+let texto = "";
 for (let m = 1; m <= t; m++) {
     r = c * (1 + (j / 100));
     c = r;
-    document.write("Mes " + m + " - valor: " + moeda(r) + "<br>");
+    texto +=  m + " : " + moeda(r) + "<br>"
+   // document.write("Mes " + m + " - valor: " + moeda(r) + "<br>");
 
 }
-document.write("resultado: " + moeda(r));
+document.getElementById("listameses").innerHTML = texto;
+document.getElementById("total").innerHTML = moeda(r);
+//document.write("resultado: " + moeda(r));
 }
 
 let op = "";
